9e2987a39678f8fc9a0a04e922cddcb0f6529852
IsaacXB <isachatr@outlook.com>
2022-04-12 13:01:37 -0600

    basic if example

--- TutorialsC/TutorialsC/Source.cpp
+++ TutorialsC/TutorialsC/Source.cpp
@@ -7,7 +7,22 @@ int main()
 	//using namespace std, the :: is not longer required for that library functions such as cout and endl
 	//std::cout << "You Died!\n";
 	cout << "You Died!" << endl;
+
+	int a = 1;
+	int b = 2;
 	
+	if (a < b)
+	{
+		cout << "a is less than b" << endl;
+	}
+	else if (a == b)
+	{
+		cout << "a is equal to b" << endl;
+	}
+	else
+	{
+		cout << "b is less than a" << endl;
+	}
 	system("pause");
 
 }
