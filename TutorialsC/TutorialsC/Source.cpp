da5a23d6d53a44f1d1b0db26cb3b0560fb32e3f5
IsaacXB <isachatr@outlook.com>
2022-04-12 01:33:56 -0600

    gitignore

--- TutorialsC/TutorialsC/Source.cpp
+++ TutorialsC/TutorialsC/Source.cpp
@@ -7,7 +7,7 @@ int main()
 	//using namespace std, the :: is not longer required for that library functions such as cout and endl
 	//std::cout << "You Died!\n";
 	cout << "You Died!" << endl;
-
+	
 	system("pause");
 
 }
